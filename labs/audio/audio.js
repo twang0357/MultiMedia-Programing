70f460a06a84e64de856be971d8bb01ed6232d52
twang0357 <twang0357@gmail.com>
2024-05-10 20:17:04 -0400

    sound

---
+++ labs/audio/audio.js
@@ -0,0 +1,25 @@
+const bird = document.getElementById("birdSound");
+const dog = document.getElementById("dogSound");
+const cat = document.getElementById("catSound");
+const horse = document.getElementById("horseSound");
+const snake = document.getElementById("snakeSound");
+
+function birdSound() {
+    bird.play();
+}
+
+function dogSound() {
+    dog.play();
+}
+
+function catSound() {
+    cat.play();
+}
+
+function horseSound() {
+    horse.play();
+}
+
+function snakeSound() {
+    snake.play();
+}
